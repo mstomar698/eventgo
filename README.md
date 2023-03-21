c03a4f9b2af8da270fdde1968cb3fed112d6d4a9
mstomar698 <tomarm698@gmail.com>
2023-03-22 01:16:15 +0530

    starter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # eventgo
+
 An visualized event planner to help planning a better event.
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-3xl text-red-500">
+      <h1>Hello starter</h1>
+    </div>
+  );
+}
